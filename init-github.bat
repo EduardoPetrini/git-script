e0c7d7bf3119b7b835b964b5c14afaa9f7165780
Eduardo Petrini <eduardo.petrini@outlook.com>
2022-10-07 15:55:53 -0300

    first commit

---
+++ init-github.bat
@@ -0,0 +1,18 @@
+
+for /f %%q in ("%CD%") do set REPO_NAME=%%~nxq
+
+echo %REPO_NAME%
+set USERNAME=EduardoPetrini
+set token=%GITHUB_TOKEN%
+
+curl -X POST -H "Accept: application/vnd.github+json" -H "Authorization: Bearer %GITHUB_TOKEN%" https://api.github.com/user/repos -d "{\"name\": \"%REPO_NAME%\", \"description\": \"%REPO_NAME%\"}"
+
+touch README.md
+echo node_modules > .gitignore
+echo .env >> .gitignore
+
+git init
+git add .
+git commit -m "first commit"
+git remote add origin git@github.com:EduardoPetrini/%REPO_NAME%.git
+git push -u origin master
